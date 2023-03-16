69467a1d44bf06d0dd92ecd6aa43f656a8809c1e
Bre3n <41246483+Bre3n@users.noreply.github.com>
2023-03-16 14:02:23 +0100

    Delete cache directory

--- cache/compiled/files/4a08ec7b6d40684998886c6b3715c58e.yaml.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/xampp/htdocs/eu/user/data/feed/c84258e9c39059a89ab77d846ddab909.yaml',
-    'modified' => 1678827717,
-    'size' => 1734,
-    'data' => NULL
-];
